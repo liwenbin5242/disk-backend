8eac5408abcd67bb403c8f2bbfe98744a50a6671
‘lwb‘ <294723284@qq.com>
2023-05-11 10:58:48 +0800

    add.

---
+++ Readme.md
@@ -0,0 +1,16 @@
+运行环境依赖:
+    centos
+    宝塔面板
+    nodejs
+    nginx
+    pgsql
+    redis
+    mongodb
+1.代码依赖
+    disk-backend
+    disk-client
+    disk-mp
+2.配置文件
+    disk-backend config下 
+    PG数据库密码确认
+    BaiDuDisk配置确认
--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const proxy = require('./lib/middlewares/proxy')
 const { logger } = require('./utils/logger');
 const { tokenAuth } = require('./lib/auth');
-const { responseTime, urlecodes, ipControl } = require('./lib/utils');
+const { responseTime, urlecodes, ipControl, machidAuth } = require('./lib/utils');
 
 const userRotes = require('./routes/user');
 const diskRotes = require('./routes/disk');
@@ -14,18 +14,18 @@ const corsRotes = require('./routes/cors');
 const wechatRotes = require('./routes/wechat');
 const memberRotes = require('./routes/member');
 const filesRotes = require('./routes/files');
+const agentRotes = require('./routes/agent');
 const corsV2Rotes = require('./routes/cors_v2');
 
 const app = express();
 app.use(cors());
 
-// 记录响应时间
 app.use('*', responseTime());
+app.use('*', machidAuth());
 app.use(urlecodes);
 
 app.use('/api/member/m3u8', ipControl(), proxy.do());
 
-// view engine setup
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: true, limit: '50mb'}));
 app.use(cookieParser());
@@ -33,10 +33,8 @@ app.all('*', (req, res, next) => {
     logger.info(`Method:${req.method} from ${req.ip.slice(7)} url:${req.path} \n query:${JSON.stringify(req.query)} \n params:${JSON.stringify(req.params)} \n body:${JSON.stringify(req.body)}`);
     next();
 });
-// 登录验证
 app.all('*', tokenAuth);
 
-// 静态资源
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'static')));
 
@@ -45,11 +43,10 @@ app.use('/api/disk', diskRotes);
 app.use('/api/wechat', wechatRotes);
 app.use('/api/member', memberRotes);
 app.use('/api/files', filesRotes);
+app.use('/api/agent', agentRotes);
 
-/**获取文件信息,从api获取 */
 app.use('/api/cors', corsRotes);
 
-/**获取文件信息,从db获取 */
 app.use('/api/cors_v2', corsV2Rotes);
 
 // catch 404 and forward to error handler
@@ -59,12 +56,9 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res) {
-    // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-    // render the error page
     res.status(err.status || 500);
     res.render('error');
 });
-
 module.exports = app;
