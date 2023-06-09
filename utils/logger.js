480df034dc8a44a20b655b353b159e497fd07e95
liwenbin <294723284@qq.com>
2023-06-09 11:24:45 +0800

    更新升级日志模块

--- utils/logger.js
+++ utils/logger.js
@@ -1,33 +1,23 @@
 const log4js = require('log4js');
 const config = require('config');
-const _ = require('lodash');
-const fs = require('fs')
+const fs = require('fs');
 
-/**创建日志文件夹 */
-if(!fs.existsSync('./logs')) fs.mkdirSync('./logs')
+/** 创建日志文件夹 */
+if (!fs.existsSync('./logs')) fs.mkdirSync('./logs');
 
 log4js.configure({
-    appenders: [
-        {
-            type: 'console',
-            category: 'console',
-        },
-        {
-            type: 'dateFile',
-            filename: `logs/${config.get('app.name')}.log`,
+    appenders: {
+        console: { type: 'console' },
+        file: { 
+            type: 'file', filename: `logs/${config.get('app.name')}.log`,
             pattern: '_yyyy-MM-dd',
             alwaysIncludePattern: false,
-            category: 'dateFileLog',
         },
-    ],
-    replaceConsole: true,
-    levels: {
-        dateFileLog: 'info',
     },
+    categories: {
+        default: { appenders: ['console', 'file'], level: 'info' }
+    }
 });
 
-const dateFileLog = log4js.getLogger(
-    _.isEqual(config.get('app.env'), 'dev') ? 'console' : 'dateFileLog'
-);
-
+const dateFileLog = log4js.getLogger();
 exports.logger = dateFileLog;
